6b1f8a991408d8887959d5cf445d2d0ef785f053
FudSy <matartevit@gmail.com>
2025-11-06 18:27:20 +0300

    added cfg realization

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,30 @@
 package config
 
-import "github.com/FudSy/DevVault/internal/pkg/postgres"
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+	"github.com/FudSy/DevVault/internal/pkg/postgres"
+)
 
 type Config struct {
 	Postgres postgres.Config
 }
 
 func InitCfg() (cfg *Config){
-	return
+	err := godotenv.Load()
+  	if err != nil {
+    	log.Fatal("Error loading .env file")
+  	}
+	cfg = &Config{
+		Postgres: postgres.Config{
+			DBName: os.Getenv("PG_DBName"),
+			Host: os.Getenv("PG_Host"),
+			Password: os.Getenv("PG_Password"),
+			Port: os.Getenv("PG_Port"),
+			SSLMode: os.Getenv("PG_SSLMode"),
+			User: os.Getenv("PG_User"),
+		},
+	}
+	return cfg
 }
